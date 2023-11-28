5bf7579daadbd23d5ebf4b0d66e383875b603744
sangho-jeon <jeonsangho1587@gmail.com>
2023-11-28 20:22:20 +0900

    [feat] gitignore added

---
+++ src/test/java/com/sangho/vthreadapp/VthreadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangho.vthreadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VthreadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
